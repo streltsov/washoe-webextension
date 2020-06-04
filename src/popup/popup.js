9f0be3cda0c5a7f96e58fb07571977dba64d4ab0
Herman Streltsov <mail@streltsov.dev>
2020-06-05 00:22:15 +0300

    Clean up

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,22 +1,14 @@
-import React, { useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
 import { Provider, useSelector } from "react-redux";
 import { Store } from "webext-redux";
-import { useForm } from "react-hook-form";
-import io from "socket.io-client";
 
 import LogIn from "./LogIn";
 import SignUp from "./SignUp";
 import AddWord from "./AddWord";
 
-const getFormValues = event =>
-  Array.from(event.target.elements)
-    .filter(el => el.name)
-    .reduce((acc, cur) => ({ ...acc, [cur.name]: cur.value }), {});
-
 const App = () => {
   const { isLoggedIn } = useSelector(x => x);
-  console.log({ isLoggedIn });
 
   return (
     <div>
