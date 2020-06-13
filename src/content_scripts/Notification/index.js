58edb11fbe3e224a7dbd6101a925d09153575d95
Herman Streltsov <mail@streltsov.dev>
2020-06-13 21:38:29 +0300

    Use className instead of id

--- src/content_scripts/Notification/index.js
+++ src/content_scripts/Notification/index.js
@@ -1,6 +1,9 @@
+import { NOTIFICATION_CLASS_NAME } from "../../constants";
+import "./style.css";
+
 function Notification (children) {
   const Notification = document.createElement("div");
-  Notification.id = "washoe-notification";
+  Notification.className = NOTIFICATION_CLASS_NAME;
   Notification.append(children);
   return Notification;
 };
