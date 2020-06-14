94f63fb8a468394f419b5f0a5c023a5b3f51f0e3
Herman Streltsov <mail@streltsov.dev>
2020-06-14 14:28:51 +0300

    Inject styles into Notification

--- src/content_scripts/Notification/index.js
+++ src/content_scripts/Notification/index.js
@@ -1,11 +1,29 @@
 import { NOTIFICATION_CLASS_NAME } from "../../constants";
-import "./style.css";
 
 function Notification (children) {
   const Notification = document.createElement("div");
   Notification.className = NOTIFICATION_CLASS_NAME;
+
+  // Styles
+  const style = document.createElement("style");
+  style.textContent = styles();
+  Notification.append(style);
+
   Notification.append(children);
   return Notification;
 };
+
+function styles() {
+  return `
+    .washoe-notification {
+      position: absolute;
+      bottom: 8px;
+      right: 8px;
+    
+      background-color: #2a2a2e;
+      padding: 8px 12px;
+      border-radius: 4px;
+    }`
+}
 
 export default Notification;
