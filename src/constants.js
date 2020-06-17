004e0cee42321cbb11d3b6e5c2a8fb8b4b028e3a
Herman Streltsov <mail@streltsov.dev>
2020-06-17 19:41:50 +0300

    Create NOTIFICATION_SHADOW_DOM_ID constant

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const INTERVALS = [ 12e4, 6e5, 36e5, 18e6, 864e5, 432e6, 10368e5, 216e7, 10368e6 ];
+export const NOTIFICATION_SHADOW_DOM_ID = "washoe-notification-shadow-dom";
 export const NOTIFICATION_CLASS_NAME = "washoe-notification";
 export const NOTIFICATION_HIDE_TIME = 0.6;
