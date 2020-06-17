2531b6fc10bc78a950101396242565b38019ae43
Herman Streltsov <mail@streltsov.dev>
2020-06-17 20:27:11 +0300

    Don't show notification if one already present on page

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,19 +1,15 @@
+import { isNotificationOnPage, createShadowDom } from "../utils/dom";
+import { NOTIFICATION_SHADOW_DOM_ID, INTERVALS } from "../constants";
 import EnterWord from "./EnterWord";
-import { isElementOnPage } from "../utils/dom";
-import { NOTIFICATION_CLASS_NAME, INTERVALS } from "../constants";
-
-const removeNotificationContainer = () =>
-  document.querySelector(NOTIFICATION_CLASS_NAME).remove();
 
 browser.runtime.onMessage.addListener(request => {
-  if (!isElementOnPage(NOTIFICATION_CLASS_NAME)) {
-    return new Promise(resolve => {
-
+  if (!isNotificationOnPage()) {
+    return new Promise( resolve => {
       const word = JSON.parse(request);
 
-      document.body.appendChild(
-        createShadowDom("SHADOW") (
-          EnterWord ( resolve ) ( word )
+      document.body.appendChild (
+        createShadowDom (NOTIFICATION_SHADOW_DOM_ID) (
+          EnterWord (resolve) (word)
         )
       );
 
