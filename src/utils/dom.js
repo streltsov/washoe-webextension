51d094aea708d23737cb2ff27a64ba9250eb6b55
Herman Streltsov <mail@streltsov.dev>
2020-06-17 19:45:01 +0300

    Create removeNotification function

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,1 +1,8 @@
-export const isElementOnPage = element => Boolean(document.querySelector(element));
+import { NOTIFICATION_SHADOW_DOM_ID } from "../constants";
+
+export const isElementOnPage = element =>
+  Boolean(document.querySelector(element));
+
+export const removeNotification = () =>
+  document.querySelector(NOTIFICATION_SHADOW_DOM_ID).remove();
+
