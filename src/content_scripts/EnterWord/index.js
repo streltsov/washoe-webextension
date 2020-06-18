82170327e09753165dd40764e2105b497b90d7dd
Herman Streltsov <mail@streltsov.dev>
2020-06-18 21:13:02 +0300

    Reset word on reset button click

--- src/content_scripts/EnterWord/index.js
+++ src/content_scripts/EnterWord/index.js
@@ -55,6 +55,10 @@ function EnterWord (resolve) {
 
     // Button
     const button = document.createElement("button");
+    button.addEventListener("click", () => {
+      resolve({ type: "reset", ...word, notifyIn: INTERVALS[0] });
+      console.info(JSON.stringify(word, 0, 2));
+    });
     button.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 16 16\"><path fill=\"rgba(249, 249, 250, .8)\" d=\"M6.854 10.854l2-2A.5.5 0 0 0 9 8.5v-4a.5.5 0 0 0-1 0v3.793l-1.854 1.853a.5.5 0 1 0 .707.707zM8 0a8.011 8.011 0 0 0-7 4.184V1.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1H2.344a.938.938 0 0 0 .056-.085 6 6 0 1 1 0 4.184 1 1 0 0 0-1.873.7A7.991 7.991 0 1 0 8 0z\"></path></svg>";
     Notification.appendChild(button);
 
