b7757dc788edb7c753b93dd718de6cb59892d306
Herman Streltsov <mail@streltsov.dev>
2020-07-13 21:55:28 +0300

    Open add word drawer on menu item click

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,8 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import AddWord from "./AddWordModal";
+
 import { isNotificationOnPage, createShadowDom } from "../utils/dom";
 import { NOTIFICATION_SHADOW_DOM_ID, INTERVALS } from "../constants";
 import EnterWord from "./EnterWord";
 
-const onShowWordModal = () => console.log("Show add word modal");
+const onShowWordModal = () => {
+  console.log("Show add word modal");
+  const washoePortal = document.createElement("div");
+  washoePortal.id = "washoe-portal";
+  document.body.appendChild(washoePortal);
+
+  ReactDOM.render(<AddWord />, document.querySelector("#washoe-portal"));
+};
+
 
 const actions = {
   "show add word modal": onShowWordModal
