a6890d96f1ad01a005de9cd20b345123ee190127
Herman Streltsov <mail@streltsov.dev>
2020-07-13 12:59:24 +0300

    Clear token on log out

--- src/background/background.js
+++ src/background/background.js
@@ -3,6 +3,8 @@ const io = require("socket.io-client");
 import { createStore, combineReducers } from "redux";
 import { wrapStore } from "webext-redux";
 
+const storage = browser.storage.local;
+
 const onLogin = data => {
   const socket = io(process.env.HOST);
   socket.on("connect", () => socket.emit("login", JSON.stringify(data)));
@@ -12,24 +14,28 @@ const onLogin = data => {
 const onAddWord = data =>
   socket.emit("add word", JSON.stringify({ ...data, notifyIn: 12e4 }));
 
+const onLogOut = () => {
+  console.log("onLogOut");
+  browser.storage.local.remove("token").then(console.log);
+};
+
 const actions = {
   login: onLogin,
-  "add word": onAddWord
+  "add word": onAddWord,
+  logout: onLogOut
 };
 
 browser.runtime.onMessage.addListener(({ action, data }) => actions[action](data));
 
 const isLoggedIn = (state = false, action) =>
   action.type == "IS_LOGGED_IN" ? action.payload : state;
-
 const rootReducer = combineReducers({ isLoggedIn });
-
 const store = createStore(rootReducer);
-
 wrapStore(store);
 
 const socket = io(process.env.HOST);
 
+// CONNECT
 socket.on("connect", () => {
   console.log("Connected, " + new Date());
   browser.storage.local.get("token").then(res => {
@@ -37,6 +43,26 @@ socket.on("connect", () => {
   });
 });
 
+// AUTHENTICATION
+socket.on("authenticated", () => {
+  console.log("Authenticated!");
+  store.dispatch({ type: "IS_LOGGED_IN", payload: true });
+});
+
+// UNAUTHORIZED
+socket.on("unauthorized", msg => {
+  console.count(`Unauthorized: ${JSON.stringify(msg.data)}`);
+//  store.dispatch({ type: "IS_LOGGED_IN", payload: false });
+});
+
+// TOKEN CHANGE
+browser.storage.onChanged.addListener( ({ token: { oldValue, newValue } }) =>
+  newValue
+    ? socket.close().connect()
+    : store.dispatch({ type: "IS_LOGGED_IN", payload: false })
+);
+
+// ON WORD
 socket.on("word", word => {
   const onRespond = res => {
     res && socket.emit("notification-response", JSON.stringify(res));
@@ -44,23 +70,10 @@ socket.on("word", word => {
   getActiveTab()
     .then(res => res.length && sendMsgToTab(res[0].id)(word).then(onRespond));
 })
-  .on("authenticated", () => {
-    console.log("Authenticated!");
-    store.dispatch({ type: "IS_LOGGED_IN", payload: true });
-  })
-  .on("unauthorized", msg => {
-    console.count(`Unauthorized: ${JSON.stringify(msg.data)}`);
-    store.dispatch({ type: "IS_LOGGED_IN", payload: false });
-  })
   .on("disconnect", msg => {
-    console.log("Disconnected, ", new Date());
-    store.dispatch({ type: "IS_LOGGED_IN", payload: false });
+    //    console.log("Disconnected, ", new Date());
+    //    store.dispatch({ type: "IS_LOGGED_IN", payload: false });
   });
-
-browser.storage.onChanged.addListener( changedProps => {
-  console.log("Token changed");
-  if (changedProps.hasOwnProperty("token")) socket.close().connect();
-});
 
 const sendMsgToTab = tabId => msg => browser.tabs.sendMessage( tabId, msg);
 const getActiveTab = () => browser.tabs.query({ currentWindow: true, active: true });
