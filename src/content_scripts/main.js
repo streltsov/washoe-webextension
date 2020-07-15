19d5381c54f67fbc2ecf718a2d60e3cbdc017755
Herman Streltsov <mail@streltsov.dev>
2020-07-15 14:07:38 +0300

    Put business logic into Washoe component

--- src/content_scripts/main.js
+++ src/content_scripts/main.js
@@ -1,28 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import AddWord from "./AddWordModal";
+import { Button, toaster } from "evergreen-ui";
 
 import { isNotificationOnPage, createShadowDom } from "../utils/dom";
 import { NOTIFICATION_SHADOW_DOM_ID, INTERVALS } from "../constants";
 import EnterWord from "./EnterWord";
 
-const onShowWordModal = () => {
-  console.log("Show add word modal");
-  const washoePortal = document.createElement("div");
-  washoePortal.id = "washoe-portal";
-  document.body.appendChild(washoePortal);
+function Washoe () {
+  const [ isDrawerOpen, setIsDrawerOpen ] = useState(false);
+  const actions = {
+    "show add word modal": () => setIsDrawerOpen(true),
+    notification: word => toaster.notify(JSON.parse(word).meaning)
+  };
 
-  ReactDOM.render(<AddWord />, document.querySelector("#washoe-portal"));
+  browser.runtime.onMessage.addListener(({ msg: { action, data } }) => actions[action](data));
+
+  return (
+    <>
+      <AddWord isShown={isDrawerOpen} onCloseComplete={() => setIsDrawerOpen(false)} />
+    </>
+  );
 };
 
-const onNotification = data => {
-  console.log("onNotification: ", data);
-};
-
-
-const actions = {
-  "show add word modal": onShowWordModal,
-  notification: onNotification
-};
-
-browser.runtime.onMessage.addListener(({ msg: { action, data } }) => actions[action](data));
+const washoeRoot = document.createElement("div");
+document.body.appendChild(washoeRoot);
+ReactDOM.render(<Washoe />, washoeRoot);
