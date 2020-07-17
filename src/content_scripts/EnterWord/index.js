4e645acec0ade752fcfbe94ea21866d73b3c2643
Herman Streltsov <mail@streltsov.dev>
2020-07-17 10:46:06 +0300

    Use evergreen-ui for notification

--- src/content_scripts/EnterWord/index.js
+++ src/content_scripts/EnterWord/index.js
@@ -1,150 +1,24 @@
-import { INTERVALS, NOTIFICATION_CLASS_NAME, WORD_REMINDER_SHADOW_DOM_ID, NOTIFICATION_HIDE_TIME } from "../../constants";
-import { removeNotification, createShadowDom } from "../../utils/dom";
-import WordReminderPopup from "../WordReminderPopup";
+import React from "react";
+import { Pane, Text, TextInput } from "evergreen-ui";
 
-const hideAndDestroy = notification => {
-  notification.classList.add("hide");
-  setTimeout(removeNotification, NOTIFICATION_HIDE_TIME * 1000);
-};
+const EnterWord = ({ word: { meaning } }) => (
+  <Pane
+    elevation="3"
+    margin="12px"
+    padding="12px"
+    display="flex"
+    position="fixed"
+    bottom="12px"
+    right="12px"
+    maxWidth="650px"
+    borderRadius="2"
+    alignItems="center"
+    background="blueTint">
+    <Text marginRight="8px">
+      {meaning}
+    </Text>
+    <TextInput autoFocus />
+  </Pane>
+);
 
-const onError = input => {
-  input.classList.add("error");
-
-  const clearError = event => {
-    input.classList.remove("error");
-    input.removeEventListener("input", clearError);
-  };
-  input.addEventListener("input", clearError);
-};
-
-function EnterWord (resolve) {
-  return function (word) {
-    const Notification = document.createElement("div");
-    Notification.className = NOTIFICATION_CLASS_NAME;
-
-    // Styles
-    const style = document.createElement("style");
-    style.textContent = styles();
-    Notification.appendChild(style);
-
-    // Meaning
-    const span = document.createElement("span");
-    span.textContent = word.meaning;
-    span.className = "meaning";
-    Notification.appendChild(span);
-
-    // Input
-    let attemptsNumber = 0;
-    const input = document.createElement("input");
-
-    input.addEventListener("keydown", event => event.stopPropagation());
-    input.addEventListener("keyup", event => event.stopPropagation());
-    input.addEventListener("keypress", event => {
-      event.stopPropagation();
-      if ( event.keyCode == 13) {
-        if ( event.target.value.toLowerCase().trim() == word.word.toLowerCase().trim() ) {
-          resolve({ type: "stageup", ...word, notifyIn: INTERVALS[ word.stage + 1 ] });
-          hideAndDestroy(Notification);
-        } else if (attemptsNumber == 2) {
-          resolve({ type: "reset", ...word, notifyIn: INTERVALS[0] });
-          hideAndDestroy(Notification);
-          document.body.append (
-            createShadowDom ( WORD_REMINDER_SHADOW_DOM_ID ) (
-              WordReminderPopup(word)
-            )
-          );
-        } else {
-          attemptsNumber = attemptsNumber + 1;
-          onError(input);
-        }
-      }
-    });
-    setTimeout(() => input.focus(), 0);
-    Notification.appendChild(input);
-
-    // Button
-    const button = document.createElement("button");
-    button.addEventListener("click", () => {
-      resolve({ type: "reset", ...word, notifyIn: INTERVALS[0] });
-      hideAndDestroy(Notification);
-      document.body.append (
-        createShadowDom ( WORD_REMINDER_SHADOW_DOM_ID ) (
-          WordReminderPopup(word)
-        )
-      );
-    });
-    button.innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"16\" height=\"16\" viewBox=\"0 0 16 16\"><path fill=\"rgba(249, 249, 250, .8)\" d=\"M6.854 10.854l2-2A.5.5 0 0 0 9 8.5v-4a.5.5 0 0 0-1 0v3.793l-1.854 1.853a.5.5 0 1 0 .707.707zM8 0a8.011 8.011 0 0 0-7 4.184V1.5a.5.5 0 0 0-1 0v5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 0-1H2.344a.938.938 0 0 0 .056-.085 6 6 0 1 1 0 4.184 1 1 0 0 0-1.873.7A7.991 7.991 0 1 0 8 0z\"></path></svg>";
-    Notification.appendChild(button);
-
-    return Notification;
-  };
-};
-
-function styles () {
-  return `
-.${NOTIFICATION_CLASS_NAME} {
-  position: fixed;
-  bottom: 8px;
-  right: 8px;
-
-  border: 1px solid rgba(249, 249, 250, 0.2);
-  background-color: #2a2a2e;
-  padding: 8px 12px;
-  border-radius: 4px;
-  z-index: 2147483647
-}
-.${NOTIFICATION_CLASS_NAME} {
-  display: flex;
-  align-items: center;
-}
-
-.${NOTIFICATION_CLASS_NAME} .meaning {
-  font-family: sans-serif;
-  color: #fff;
-}
-
-.${NOTIFICATION_CLASS_NAME} input {
-  background-color: #38383d;
-  border: 1px solid #5f5f63;
-  color: #fff;
-  padding: 8px;
-  border-radius: 3px;
-  margin: 0 8px;
-}
-
-.${NOTIFICATION_CLASS_NAME} button {
-  all: unset;
-  width: 32px;
-  height: 32px;
-  display: flex;
-  border-radius: 2px;
-  align-items: center;
-  justify-content: center;
-  box-sizing: border-box;
-}
-
-.${NOTIFICATION_CLASS_NAME} input:focus,
-.${NOTIFICATION_CLASS_NAME} button:focus {
-  border: 1px solid #0a84ff;
-  box-shadow: 0 0 0 1px #0a84ff, 0 0 0 4px rgba(10, 132, 255, 0.3);
-}
-
-.${NOTIFICATION_CLASS_NAME} input.error {
-  border: 1px solid #d70022;
-  box-shadow: 0 0 0 1px #d70022, 0 0 0 4px rgba(251, 0, 34, 0.3);
-}
-
-.${NOTIFICATION_CLASS_NAME}.hide {
-  animation-name: hide;
-  animation-duration: ${NOTIFICATION_HIDE_TIME}s;
-  animation-fill-mode: forwards;
-}
-
-@keyframes hide {
-  0% { bottom: 8px; }
-  25% { bottom: 24px; }
-  100% { bottom: -72px; }
-}`;
-}
-
-export default EnterWord;
+export { EnterWord };
--- src/content_scripts/Washoe/index.js
+++ src/content_scripts/Washoe/index.js
@@ -1,29 +1,22 @@
 import React, { useState } from "react";
 import AddWord from "../AddWordModal";
-import { Pane, Text, TextInput, Button, toaster } from "evergreen-ui";
+import { EnterWord } from "../EnterWord";
 import "./style.css";
 
-//import { isNotificationOnPage, createShadowDom } from "../utils/dom";
-//import { NOTIFICATION_SHADOW_DOM_ID, INTERVALS } from "../constants";
-
-const EnterWord = ({ word }) => (
-  <Pane display="flex" zIndex="2147483647">
-    <Text>{JSON.parse(word).meaning}</Text>
-    <TextInput />
-  </Pane>
-);
-
 function Washoe () {
+  const [ word, setWord ] = useState({});
   const [ isDrawerOpen, setIsDrawerOpen ] = useState(false);
+
   const actions = {
     "show add word modal": () => setIsDrawerOpen(true),
-    notification: word => toaster.notify(<EnterWord word={word} />, { duration: 9e9, position: "bottom" })
+    notification: word => setWord(JSON.parse(word))
   };
 
   browser.runtime.onMessage.addListener(({ msg: { action, data } }) => actions[action](data));
 
   return (
     <>
+      { Object.keys(word).length ? <EnterWord word={word}/> : null }
       <AddWord isShown={isDrawerOpen} onCloseComplete={() => setIsDrawerOpen(false)} />
     </>
   );
