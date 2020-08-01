4aed7da6c5ce57a0f9d8271f5657e56f9add0db6
Herman Streltsov <mail@streltsov.dev>
2020-08-01 22:38:47 +0300

    Create post function

--- src/background/api.js
+++ src/background/api.js
@@ -4,3 +4,5 @@ const instance = axios.create({
   baseURL: process.env.BASE_URL + "/api/v1",
   headers: { "Content-Type": "application/json" }
 });
+
+export const post = url => data => instance.post(url, data);
