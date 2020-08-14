aca91be8af30630e9f92734b17cf0592ceecbae2
Herman Streltsov <mail@streltsov.dev>
2020-08-15 01:15:51 +0300

    Set id as key for word in IndexedDB

--- src/background/background.js
+++ src/background/background.js
@@ -2,17 +2,8 @@ import { sendMessageToActiveTab } from "../utils/webExtension";
 import { INTERVALS } from "../constants";
 import localforage from "localforage";
 
+import { mergeMap } from "rxjs/operators";
 import { from, interval } from "rxjs";
-import { map, mergeMap, tap } from "rxjs/operators";
-
-
-const showAddWordForm = _ =>
-  sendMessageToActiveTab({ action: "showAddWordForm" });
-
-const addWord = data =>
-  localforage.setItem(data.word, { id: Date.now(), ...data, stage: 0, notifyOn: Date.now() + INTERVALS[0] })
-    .then(_ => sendMessageToActiveTab({ action: "closeAddWordForm" }))
-    .catch(error => console.error("addWord -> localforage.setItem: ", error));
 
 const getWordToRepeat = _ => new Promise(resolve =>
   localforage.iterate(({ notifyOn, ...rest }) => notifyOn < Date.now() ? resolve(rest) : null)
@@ -22,7 +13,17 @@ const subscribe = interval(5000)
   .pipe(mergeMap(_ => from(getWordToRepeat())))
   .subscribe(data => sendMessageToActiveTab({ action: "notification", data }));
 
+const showAddWordForm = _ =>
+  sendMessageToActiveTab({ action: "showAddWordForm" });
+
+const addWord = data => {
+  const id = Date.now();
+  localforage.setItem(id, { id, ...data, stage: 0, notifyOn: Date.now() + INTERVALS[0] })
+    .then(_ => sendMessageToActiveTab({ action: "closeAddWordForm" }))
+    .catch(error => console.error("addWord -> localforage.setItem: ", error));
+
 const actions = {
+  incrementWordStage,
   showAddWordForm,
   addWord
 };
