6ba39ef424677ccee98ba9cf008bc63229d0566d
Herman Streltsov <mail@streltsov.dev>
2020-08-28 18:17:03 +0300

    Clean up

--- src/content_scripts/Washoe/index.js
+++ src/content_scripts/Washoe/index.js
@@ -17,15 +17,11 @@ function Washoe () {
   browser.runtime.onMessage.addListener(({ action, data }) => actions[action](data));
 
   function onFail () {
-    console.log("On Fail from Washoe component!");
-    console.log("Word: ", word);
     browser.runtime.sendMessage({ action: "resetWordStage", data: { id: word.id } });
     setWord({});
   };
 
   function onSuccess () {
-    console.log("On Success from Washoe component!");
-    console.log("Word: ", word);
     browser.runtime.sendMessage({ action: "incrementWordStage", data: { id: word.id } });
     setWord({});
   };
