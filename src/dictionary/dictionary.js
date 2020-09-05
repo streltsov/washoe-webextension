30ef8a4ffe6b3e9e23001ee4e31d69dced2f4ba6
Herman Streltsov <mail@streltsov.dev>
2020-09-06 00:36:33 +0300

    Set up ErrorBoundary

--- src/dictionary/dictionary.js
+++ src/dictionary/dictionary.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
+import { ErrorFallback } from "../components/ErrorFallback";
 import { fetchAllWords } from "../utils/webExtension";
+import { ErrorBoundary } from "react-error-boundary";
 import { Table } from "./components/Table";
 
 function Dictionary () {
@@ -9,4 +11,9 @@ function Dictionary () {
   return <Table words={words} />;
 };
 
-ReactDOM.render(<Dictionary />, document.getElementById("root"));
+ReactDOM.render(
+  <ErrorBoundary FallbackComponent={ErrorFallback}>
+    <Dictionary />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
