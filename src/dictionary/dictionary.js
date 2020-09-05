4c8ba884ee7612653158a0b43c6bf2e1236adc3f
Herman Streltsov <mail@streltsov.dev>
2020-09-05 17:30:50 +0300

    Create dictionary table

--- src/dictionary/dictionary.js
+++ src/dictionary/dictionary.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { Pane } from "evergreen-ui";
+import { Pane, Table } from "evergreen-ui";
+import localforage from "localforage";
+
+
 
 function Dictionary () {
-  console.log("helluva");
+  const [ words, setWords ] = useState([]);
+
+  useEffect(_ => {
+    localforage.iterate(function (value, key, iterationNumber) {
+      setWords(p => [ ...p, value ]);
+    }).then(function () {
+      console.log("Iteration has completed");
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }, []);
+
+  useEffect(_ => console.log(words), [ words ]);
+
   return (
     <Pane>
-      Hello!
+      <Table>
+
+        <Table.Head>
+          <Table.SearchHeaderCell />
+          <Table.TextHeaderCell>Meaning</Table.TextHeaderCell>
+          <Table.TextHeaderCell>Example</Table.TextHeaderCell>
+          <Table.TextHeaderCell>Stage</Table.TextHeaderCell>
+        </Table.Head>
+
+        <Table.Body>
+          {words.map(word => (
+            <Table.Row key={word.id}>
+              <Table.TextCell>{word.word}</Table.TextCell>
+              <Table.TextCell>{word.meaning}</Table.TextCell>
+              <Table.TextCell>{word.example}</Table.TextCell>
+              <Table.TextCell isNumber>{word.stage}</Table.TextCell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+
+      </Table>
     </Pane>
   );
 };
