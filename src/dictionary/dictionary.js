e9b0c6b1293b2c12b6568e720e95e1c5268c773d
Herman Streltsov <mail@streltsov.dev>
2020-09-05 18:17:22 +0300

    Move fetchAllWords function to utils

--- src/dictionary/dictionary.js
+++ src/dictionary/dictionary.js
@@ -1,23 +1,13 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
-import { Pane } from "evergreen-ui";
-import localforage from "localforage";
+import { fetchAllWords } from "../utils/webExtension";
 import { Table } from "./components/Table";
-
+import localforage from "localforage";
+import { Pane } from "evergreen-ui";
 
 function Dictionary () {
   const [ words, setWords ] = useState([]);
-
-  useEffect(_ => {
-    localforage.iterate(function (value, key, iterationNumber) {
-      setWords(p => [ ...p, value ]);
-    }).then(function () {
-      console.log("Iteration has completed");
-    }).catch(function (err) {
-      console.log(err);
-    });
-  }, []);
-
+  useEffect(_ => fetchAllWords(setWords), []);
   return <Table words={words} />;
 };
 
--- src/utils/webExtension.js
+++ src/utils/webExtension.js
@@ -1,3 +1,5 @@
+import localforage from "localforage";
+
 // getActiveTabId :: IO -> Promise( Array )
 export const getActiveTabId = () =>
   browser.tabs.query({ currentWindow: true, active: true }).then(tabs => tabs[0].id);
@@ -8,3 +10,11 @@ export const sendMessageToTab = tabId => msg => browser.tabs.sendMessage(tabId, 
 // sendMessageToActiveTab :: Object -> IO
 export const sendMessageToActiveTab = msg =>
   getActiveTabId().then(activeTabId => sendMessageToTab(activeTabId)(msg));
+
+// fetchAllWords :: Function -> IO
+export const fetchAllWords = cb => {
+  const words = [];
+  localforage.iterate(word => void words.push(word))
+    .then(_ => cb(words))
+    .catch(error => console.error("fetchAllWords: ", error));
+};
