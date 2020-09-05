2296e44c16df9fda314cc6d1363e7237a532d314
Herman Streltsov <mail@streltsov.dev>
2020-09-05 19:49:09 +0300

    Clean up

--- src/dictionary/dictionary.js
+++ src/dictionary/dictionary.js
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { fetchAllWords } from "../utils/webExtension";
 import { Table } from "./components/Table";
-import localforage from "localforage";
-import { Pane } from "evergreen-ui";
 
 function Dictionary () {
   const [ words, setWords ] = useState([]);
